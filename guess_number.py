68e9df3977bc3c7e5a50fe0d226de8366068d462
torrentxok <torrentxok@mail.ru>
2022-03-25 00:44:50 +0300

    Added levels

--- guess_number.py
+++ guess_number.py
@@ -1,21 +1,43 @@
 from random import *
 from math import *
 
-unknownNum = randint(1, 100)
 print("Добро пожаловать в игру \"Угадай число\"")
 
-def is_valid(text):
-    if text.isdigit() and 1<=int(text)<=100:
+def is_valid(text, level):
+    if text.isdigit() and 1<=int(text)<=level:
         return True
     else:
         return False
 
+def level_is_valid(text):
+    if text.isdigit() and int(text)>=100:
+        return True
+    else:
+        return False
+
+print('Введите уровень сложности (введите число, не меньше 100)')
+
+unknownNum = None
+levelNum = None
+
+while True:
+    levelNum = input()
+    if level_is_valid(levelNum):
+        levelNum = int(levelNum)
+        unknownNum = randint(1, levelNum)
+        break
+    else:
+        print('Ведите корректный уровень')
+
+
+print('Введите число!')
+
 while True:
     n = input()
-    if is_valid(n):
+    if is_valid(n, levelNum):
         n = int(n)
     else:
-        print('А может быть все-таки введем целое число от 1 до 100?')
+        print(f'А может быть все-таки введем целое число от 1 до {levelNum}?')
         continue
     if n > unknownNum:
         print('Ваше число больше загаданного, попробуйте еще разок')
